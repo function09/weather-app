157f38a79ec8cc62357c59162745f9560549fddc
function07 <houssine199222@gmail.com>
2023-07-08 17:55:46 -0400

    Include throw/catch to handle errors

--- src/modules/logic/API.js
+++ src/modules/logic/API.js
@@ -1,21 +1,24 @@
-async function getWeather() {
-  const response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=2d2c3fc74eb549aaba1202946230807&q=Paris",
-    {
-      mode: "cors",
+async function getWeatherData() {
+  try {
+    const response = await fetch(
+      "http://api.weatherapi.com/v1/current.json?key=2d2c3fc74eb549aaba1202946230807&q=paris",
+      {
+        mode: "cors",
+      }
+    );
+    const weatherData = await response.json();
+
+    if (!response.ok) {
+      const responseStatus = response.status;
+      const responseStatusText = response.statusText;
+
+      console.log(`Error ${responseStatus}: ${responseStatusText}`);
+    } else {
+      console.log(weatherData);
     }
-  );
-
-  const weatherData = await response.json();
-
-  if (!response.ok) {
-    const responseStatus = response.status;
-    const responseStatusText = response.statusText;
-
-    console.log(`Error ${responseStatus}: ${responseStatusText}`);
-  } else {
-    console.log(weatherData);
+  } catch (error) {
+    console.log(error);
   }
 }
 
-export default getWeather;
+export default getWeatherData;
