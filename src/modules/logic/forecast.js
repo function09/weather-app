fa6426a9545adaea09e9c122e1515835d89f6e99
function07 <houssine199222@gmail.com>
2023-07-12 20:27:58 -0400

    Add threeDayForecast property to forecastData object in collectForecastData function

--- src/modules/logic/forecast.js
+++ src/modules/logic/forecast.js
@@ -4,13 +4,14 @@ const collectForecastData = (data) => {
   const forecastData = {
     highest: data.forecast.forecastday[0].day.maxtemp_c,
     lowest: data.forecast.forecastday[0].day.mintemp_c,
+    threedayForecast: data.forecast.forecastday,
   };
   return forecastData;
 };
 
 async function fetchForecastData() {
   const forecastAPI =
-    "http://api.weatherapi.com/v1/forecast.json?key=1d38f27405c74273950235259231107&q=paris";
+    "http://api.weatherapi.com/v1/forecast.json?key=1d38f27405c74273950235259231107&q=paris&days=3";
 
   try {
     const response = await fetch(forecastAPI, { mode: "cors" });
@@ -18,6 +19,7 @@ async function fetchForecastData() {
     const dataObject = collectForecastData(data);
     displayForecast(dataObject);
     collectForecastData(data);
+    console.log(collectForecastData(data));
   } catch (error) {
     console.log(error);
   }
